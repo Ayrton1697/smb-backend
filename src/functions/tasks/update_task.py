3ce9ce896092b44a5218ee5c27da1a966ac2b28a
Ayrton ST <ayrton.steffich@gmail.com>
2023-12-11 09:20:03 -0300

    update bookings and tasks

--- src/functions/tasks/update_task.py
+++ src/functions/tasks/update_task.py
@@ -41,6 +41,14 @@ def lambda_handler(event, context):
 
     data = json.loads(event['body'])
 
+    """    
+    data["appointment_id"]
+    data["user_email"]
+    data["status"] -> nuevo status not started | in progress | completed | deleted
+    
+    Parametros 
+    """
+
     try:
         lambda_client = boto3.client('lambda', region_name='us-east-1' )
         
@@ -59,48 +67,37 @@ def lambda_handler(event, context):
         }
 
     
-    try:
-        # Check if there is an existing booking with the same email
-        response = client.query(
-        TableName='smb-appointments-test',
-        IndexName='email-index',
-        KeyConditionExpression='#mail = :email',
-        ExpressionAttributeNames={
-            '#mail': 'mail'
-        },
-        ExpressionAttributeValues={
-            ':email': {'S': data["email"]}
-        }
-        # ProjectionExpression='booking_time'
-        )
-        time_difference = []
-        time_difference = []
-        print('=============================================================')
-        print(response['Items'])
-        print(response['Items'][0])
-        if 'Items' in response:
-            print("Items found")
-            # Get the timestamp of the first booking
-            first_booking_time = datetime.datetime.strptime(response['Items'][0]['datetime']['S'], '%Y-%m-%d %H:%M')
-        
-            # Calculate the current timestamp
-            current_appointment_time = datetime.datetime.strptime(data['datetime'], '%Y-%m-%d %H:%M')
+    # try:
+    #     # Check if there is an existing booking with the same email
+    #     response = client.query(
+    #     TableName='smb-appointments-test',
+    #     IndexName='email-index',
+    #     KeyConditionExpression='#datetime_location = :datetime_location',
+    #     ExpressionAttributeNames={
+    #         '#datetime_location': 'datetime_location'
+    #     },
+    #     ExpressionAttributeValues={
+    #         ':datetime_location': {'S': data["appointment_id"]}
+    #     }
+    #     # ProjectionExpression='booking_time'
+    #     )
+   
+    #     print('=============================================================')
+    #     print(response['Items'])
+    #     print(response['Items'][0])
+    #     if 'Items' in response:
+    #         print("Items found")
 
-            # Calculate the difference between the current timestamp and the first booking timestamp
-            time_difference = current_appointment_time - first_booking_time
-            print('TIME DIFFERENCE IS: ')
-            print(time_difference)
-        # Check if the time difference is within the allowed range of 30 days
-        if time_difference.days <= 30:
-            return {
-                'statusCode': 400,
-                'headers': {},
-                'body': json.dumps({'message': 'Lo sentimos. Ya existe una reserva para ese correo en los proximos 30 días.'})
-            }
-    except Exception as e:
-        print(e)
+    #     else:
+    #         print("There were no appointments found.")
+    #         return 
+
+    
+    # except Exception as e:
+    #     print(e)
 
     print(data)
+
     try:
         print(f"{data['datetime']}_{data['location']}")
         print("INSIDE TRY BLOCK TABLE INSERT")
@@ -108,53 +105,15 @@ def lambda_handler(event, context):
         TableName='smb-appointments-test',
         Key={'datetime_location': {'S': str(f"{data['datetime']}_{data['location']}")},
             'type': {'S': data['type']  }},
-        # Item={'id' : {
-        #     'N': str(int(time.time()))
-        #         },
-        #     'USER_ID' : {
-        #     'N': data["user_id"]
-        #         },
-        #     'user_phone_number' : {
-        #     'N': data["user_phone_number"]
-        #         },
-        #     'appointment_id' : {
-        #     'N': data["appointment_id"]
-        #         },
-        #     'datetime': {
-        #         'S': data["datetime"]
-        #         },
-        #     'description' : {
-        #         'S': data["description"]
-        #         },
-        #     'optionals' : {
-        #         'S': data["optionals"]
-        #         },
-        #     'ref_code' : {
-        #         'S': data["ref_code"]
-        #         },
-        #     'type' : {
-        #         'S': data["type"]
-        #         },
-        #     'location' : {
-        #         'S': data["location"]
-        #         },
-        #     'mail' : {
-        #         'S': data["email"]
-        #         },
-        #     'CLIENT_ID' : {
-        #         'S': data["client_id"]
-        #         },
-        # },
-        UpdateExpression="SET mail = :dup_value, ref_code = :ref_code",
-        ConditionExpression='attribute_not_exists(mail) AND mail = :empty_value AND mail <> :dup_value',
+        UpdateExpression=f"SET {data['step']} = :new_status",
+        ConditionExpression='mail = :existing_email',
         ExpressionAttributeValues={
-            ':empty_value': {'S': ""},
-            ':dup_value': {'S': data["email"] },
-            ':ref_code': {'S': "CUSTOM_REF_CODE_AXZ" }
-            # ':dup_location': {'S': data["location"] }
+            ':existing_email': {'S': data["email"] },
+            ':new_status': {'S': data["status"] }
         }
         )
         print(response)
+        print("RESPONSE:" + str(response))
         print("PUT RESPONSE")
         if response['ResponseMetadata']['HTTPStatusCode'] == 200:
             pass
@@ -188,7 +147,7 @@ def lambda_handler(event, context):
             return {
             "statusCode": 400,
             "body": json.dumps({
-                "message": "Lo sentimos. Ese horario ya se encuentra reservado."
+                "message": "Lo sentimos. No se pudo modificar el estado."
                 # "location": ip.text.replace("\n", "")
             }),
          }
@@ -215,7 +174,7 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps({
             "appointment": 'response',
-            "message": "El turno fue creado exitosamente."
+            "message": "El estado del trámite fue actualizado exitosamente."
             # "location": ip.text.replace("\n", "")
         }),
     }
